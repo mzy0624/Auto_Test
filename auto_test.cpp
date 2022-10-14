e21873df046ed0c1b21c13eafdd8686a2cd5df66
mzy-0624 <191850145@smail.nju.edu.cn>
2022-10-14 10:41:14 +0800

    cpp

--- auto_test.cpp
+++ auto_test.cpp
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 int main(int argc, char *argv[])
 {
     int T = atoi(argv[1]);
     for (int i = 1; i < T; i++)
     {
-        printf("The result of No. %d case is: ", i);
+        cout << "The result of No. " << i << "case is: ";
         system("./data.out > data.in");
         system("./totest.out < data.in > totest.txt");
         system("./std.out < data.in > std.txt");
         if (system("diff std.txt totest.txt"))
         {
-            printf("\033[1;31mWrong Answer\033[0m\n");
+            cout << "\033[1;31mWrong Answer\033[0m\n";
             return 0;
         }
         else
-            printf("\033[1;32mAccepted\033[0m\n");
+            cout << "\033[1;32mAccepted\033[0m\n";
     }
     return 0;
 }
